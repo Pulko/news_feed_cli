9d4a6bc28a74767d4269d18b41149af42c907a6e
pulko <vzdbovich@gmail.com>
2024-03-30 18:46:17 +0100

    adjust news API service

--- newsapi/src/lib.rs
+++ newsapi/src/lib.rs
@@ -2,6 +2,9 @@ use std::io;
 
 use serde::Deserialize;
 use thiserror::Error;
+use url;
+
+const BASE_URL: &str = "https://newsapi.org/v2/";
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -21,16 +24,30 @@ mod tests {
 #[derive(Error, Debug)]
 pub enum NewsApiError {
     #[error("Failed to fetch data form API")]
-    RequestFailed(ureq::Error),
+    RequestFailed(#[from] ureq::Error),
     #[error("Failed to parse data")]
-    ParseFailed(serde_json::Error),
+    ParseFailed(#[from] serde_json::Error),
     #[error("Failed to convert response to string")]
-    ResponseToStringFailed(io::Error),
+    ResponseToStringFailed(#[from] io::Error),
+    #[error("Failed to parse URL")]
+    UrlParseFailed(#[from] url::ParseError),
+    #[error("Failed to prepare URL")]
+    UrlPreparingFailed,
+    #[error("Bad request: {0}")]
+    BadRequest(String),
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Data {
+pub struct NewsApiResponse {
+    pub status: String,
     pub articles: Vec<Article>,
+    pub code: Option<String>,
+}
+
+impl NewsApiResponse {
+    pub fn get_articles(&self) -> &Vec<Article> {
+        &self.articles
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -39,14 +56,121 @@ pub struct Article {
     pub url: String,
 }
 
-pub fn get_articles(url: &String) -> Result<Data, NewsApiError> {
-    let response = ureq::get(url)
-        .call()
-        .map_err(|e| NewsApiError::RequestFailed(e))?
-        .into_string()
-        .map_err(|e| NewsApiError::ResponseToStringFailed(e))?;
+impl Article {
+    pub fn title(&self) -> &str {
+        &self.title
+    }
 
-    let articles = serde_json::from_str(&response).map_err(|e| NewsApiError::ParseFailed(e))?;
+    pub fn url(&self) -> &str {
+        &self.url
+    }
+}
 
-    Ok(articles)
+pub struct NewsApi {
+    api_key: String,
+    endpoint: Endpoint,
+    country: Country,
+}
+
+pub enum Country {
+    Us,
+}
+
+impl ToString for Country {
+    fn to_string(&self) -> String {
+        match self {
+            Country::Us => String::from("us"),
+        }
+    }
+}
+
+pub enum Endpoint {
+    TopHeadlines,
+}
+
+impl ToString for Endpoint {
+    fn to_string(&self) -> String {
+        match self {
+            Endpoint::TopHeadlines => "top-headlines".to_string(),
+        }
+    }
+}
+
+impl NewsApi {
+    pub fn new(api_key: &str) -> Self {
+        let api = NewsApi {
+            api_key: api_key.to_string(),
+            endpoint: Endpoint::TopHeadlines,
+            country: Country::Us,
+        };
+
+        api
+    }
+
+    pub fn endpoint(&mut self, endpoint: Endpoint) -> &mut Self {
+        self.endpoint = endpoint;
+        self
+    }
+
+    pub fn country(&mut self, country: Country) -> &mut Self {
+        self.country = country;
+        self
+    }
+
+    fn prepare_url(&self) -> Result<String, NewsApiError> {
+        let mut url = url::Url::parse(BASE_URL).map_err(|e| NewsApiError::UrlParseFailed(e))?;
+
+        url.path_segments_mut()
+            .map_err(|_| NewsApiError::UrlPreparingFailed)?
+            .push(&self.endpoint.to_string());
+
+        let country = format!("country={}", self.country.to_string());
+        url.set_query(Some(&country));
+
+        Ok(url.to_string())
+    }
+
+    pub fn fetch(&self) -> Result<NewsApiResponse, NewsApiError> {
+        let url = self.prepare_url()?;
+        let req = ureq::get(&url).set("Authorization", &self.api_key);
+        let resp: NewsApiResponse = req.call()?.into_json()?;
+
+        match resp.status.as_str() {
+            "ok" => return Ok(resp),
+            _ => return Err(self.map_response_err(resp.code)),
+        }
+    }
+
+    fn map_response_err(&self, code: Option<String>) -> NewsApiError {
+        if let Some(code) = code {
+            match code.as_str() {
+                "apiKeyDisabled" => {
+                    return NewsApiError::BadRequest("API key is disabled".to_string())
+                }
+                "apiKeyExhausted" => {
+                    return NewsApiError::BadRequest(
+                        "API key has no more requests available".to_string(),
+                    )
+                }
+                "apiKeyInvalid" => {
+                    return NewsApiError::BadRequest("API key is invalid".to_string())
+                }
+                "apiKeyMissing" => {
+                    return NewsApiError::BadRequest("API key is missing".to_string())
+                }
+                "parameterInvalid" => {
+                    return NewsApiError::BadRequest("Parameters are invalid".to_string())
+                }
+                "parametersMissing" => {
+                    return NewsApiError::BadRequest("Parameters are missing".to_string())
+                }
+                "rateLimited" => {
+                    return NewsApiError::BadRequest("Request is rate limited".to_string())
+                }
+                _ => return NewsApiError::BadRequest("Unknown error".to_string()),
+            }
+        } else {
+            return NewsApiError::BadRequest("Unknown error".to_string());
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 mod theme;
 
 use dotenv::dotenv;
-use newsapi::{get_articles, Data};
+use newsapi::{Country, Endpoint, NewsApiResponse};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
-    let url = get_url();
-    let data: Data = get_articles(&url)?;
+    let api_key = get_api_key()?;
+    let data = newsapi::NewsApi::new(api_key.as_str())
+        .country(Country::Us)
+        .endpoint(Endpoint::TopHeadlines)
+        .fetch()?;
 
     print_articles(data);
 
     Ok(())
 }
 
-fn print_articles(data: Data) -> () {
+fn print_articles(data: NewsApiResponse) -> () {
     let theme = theme::default();
     theme.print_text("Top headlines\n\n");
-    for article in &data.articles {
-        theme.print_text(&format!("`{}`", article.title));
-        theme.print_text(&format!("> *{}*", article.url));
+    for article in data.get_articles() {
+        theme.print_text(&format!("`{}`", article.title()));
+        theme.print_text(&format!("> *{}*", article.url()));
         theme.print_text("---")
     }
 }
@@ -30,11 +32,4 @@ fn get_api_key() -> Result<String, dotenv::Error> {
     let api_key = std::env::var("NEWS_API_KEY").unwrap_or("".to_string());
 
     Ok(api_key)
-}
-
-fn get_url() -> String {
-    let api_key = get_api_key().unwrap();
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
-
-    format!("{}{}", url, api_key)
 }
